d868ca5c2f2f0d677ea3dbbb3232400b92d2d110
LucasChlz <lucasgabrielsampaio2019@gmail.com>
2020-10-07 22:27:19 -0300

    creating a clear command

---
+++ commands/clear.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'clear',
+    description: 'clean chat',
+    async execute(message, args)
+    {
+
+        const user = message.member;
+
+        if (!user.hasPermission('ADMINISTRATOR', true)) return message.channel.send(`You need to be a administrator`);
+
+        const messageNumber = args[0];
+
+        if (!messageNumber) return message.channel.send(`Please type a number between 1 and 100`);
+        if (messageNumber <= 0) return message.channel.send(`Please type a number between 1 and 100`);
+
+        const messageFetch = await message.channel.messages.fetch({ limit: messageNumber});
+
+        return message.channel.bulkDelete(messageFetch);
+    }
+}
