9f38e9c6bc06e7052093c1334c8c76fdba23fa7a
LucasChlz <lucasgabrielsampaio2019@gmail.com>
2020-10-11 01:59:24 -0300

    adjusting some stuffs in ban command

--- commands/ban.js
+++ commands/ban.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { prefix } = require('../config.json');
 
 module.exports = {
     name: 'ban',
@@ -10,18 +11,34 @@ module.exports = {
         if(!author.hasPermission('ADMINISTRATOR')) return message.channel.send('You need to be a administrator');
 
         const memberBan = message.mentions.users.first();
-        if(!memberBan) return message.channel.send(`You need to tag someone`);
-        const timeBan = args[1];
-        if(!timeBan) return message.channel.send(`type how many days had he been banned`);
 
-        return await message.guild.members.ban(memberBan, { days: timeBan })
+        if(!memberBan) {
+            const alertEmbed = new Discord.MessageEmbed()
+            .setColor(`#4a34a3`)
+            .setDescription(`${prefix}ban <user> <reason> <days>`);
+        
+            return message.channel.send(alertEmbed);
+        }
+
+        const reasonBan = args.slice(1).join(" ");
+        
+        if(!reasonBan) return message.channel.send(`What is the reason for the ban?`);
+
+        const timeBan = args[args.length - 1];
+
+        if(!timeBan) return message.channel.send(`Type how many days had he been banned`);
+        if(timeBan > 7 || timeBan < 1) return message.channel.send(`The number of days must be between 1 and 7`);
+
+        return await message.guild.members.ban(memberBan, { days: timeBan, reason: reasonBan })
             .then(() => {
                 const banMessage = new Discord.MessageEmbed()
                     .setColor('#d62747')
                     .setTitle(`${memberBan.username} has been banned for ${timeBan} days`)
-                    .setImage('');
+                    .setImage('https://media.tenor.com/images/2987e5d56e9c704a4cef8e345cef4c04/tenor.gif');
 
-               return message.channel.send(banMessage);
+            return message.channel.send(banMessage);
+            }).catch(() => {
+                return message.channel.send(`Missing permissions`);
             });
         
         
